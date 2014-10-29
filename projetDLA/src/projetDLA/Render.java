bc67bbd3a580e6238ae43ce2a7586cc2c6124674
amblard <vincent.guillebaud@gmail.com>
2014-10-29 17:17:08 +0100

    Correct bug when closing window

--- projetDLA/src/projetDLA/Render.java
+++ projetDLA/src/projetDLA/Render.java
@@ -6,7 +6,6 @@ public class Render extends JFrame {
 	public Render() {
 		this.setTitle("Diffusion Limited Aggregation");
 		this.setSize(Main.windowsWidth,Main.widowsHeight);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 
 		this.setVisible(true);
